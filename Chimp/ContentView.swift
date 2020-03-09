c147f86174006b6cacf05f676b4e21dc5f57bf24
sean <seansaoirseh@gmail.com>
2020-03-09 13:15:42 +0100

    Initial Commit

---
+++ Chimp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Chimp
+//
+//  Created by Sean Saoirse on 09/03/20.
+//  Copyright © 2020 Sean Saoirse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ChimpTests/ChimpTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChimpTests.swift
+//  ChimpTests
+//
+//  Created by Sean Saoirse on 09/03/20.
+//  Copyright © 2020 Sean Saoirse. All rights reserved.
+//
+
+import XCTest
+@testable import Chimp
+
+class ChimpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
