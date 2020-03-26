ac496ece740243112153fe04246da3486200e2f7
sean <seansaoirseh@gmail.com>
2020-03-26 11:38:46 +0100

    added tabbar in detailview

--- Chimp/View/DetailView.swift
+++ Chimp/View/DetailView.swift
@@ -3,14 +3,16 @@ import SwiftUI
 
 
 struct DetailView: View {
+    @State private var selectedTab = 0
+
     @ObservedObject private var indexManager = IndexManager()
-//      @State private var contactNameSection: [ContactSection] = []
-       var contactMethod = ["Calls","E-Mails", "Notes"]
+    //      @State private var contactNameSection: [ContactSection] = []
+    var contactMethod = ["Calls","E-Mails", "Notes"]
     
     let contact: ContactDetail
     @State private var favoriteColor = 0
     var body: some View {
-        VStack{
+        VStack(spacing:0){
             HStack{
                 VStack{
                     Image("frut")
@@ -48,15 +50,25 @@ struct DetailView: View {
                     }.padding(EdgeInsets(top: 0, leading: 16, bottom: 8, trailing: 0))
                     
                     VStack{
-                        Picker("", selection: $indexManager.contactSelectorIndex) {
-                                                   ForEach(0 ..< contactMethod.count) { index in
-                                                       Text(self.contactMethod[index]).tag(index)
-                                                   }
-                                               }.pickerStyle(SegmentedPickerStyle())
-                     
-                    }.padding(EdgeInsets(top: 32, leading: 240, bottom: 8, trailing: 240))
-                 
-                    
+//                        Picker("", selection: $indexManager.contactSelectorIndex) {
+//                            ForEach(0 ..< contactMethod.count) { index in
+//                                Text(self.contactMethod[index]).tag(index)
+//                            }
+//                        }.pickerStyle(SegmentedPickerStyle())
+                        
+                        TabView(selection: $selectedTab) {
+                               PickerSamplesView().tabItem { Text("Pickers") }.tag(0)
+                            
+                            PickerSamplesView().tabItem { Text("Pickers2") }.tag(1)
+                            
+                            PickerSamplesView().tabItem { Text("Pickers3") }.tag(2)
+                             
+                           }
+                           .frame(maxWidth: .infinity, maxHeight: .infinity)
+                           .padding()
+
+
+                    }.padding(EdgeInsets(top: 32, leading: 120, bottom: 8, trailing: 120))
                     
                 }
                 .frame(minWidth: 50, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: Alignment.topLeading)
@@ -67,7 +79,24 @@ struct DetailView: View {
                 
             }.padding(.top,32).padding(.leading,32)
             
-          
+            
+//            VStack(spacing: 0){
+//                Rectangle()
+//                .fill(Color.red)
+//                .frame(width: 200, height: 200)
+////                List {
+////
+////                    Text("Title")
+////                    Divider()
+////                    Text("Title")
+////                    Divider()
+////                    Text("Title")
+////                    Divider()
+////
+////                }
+//
+//            }
+            
         }
         
         
